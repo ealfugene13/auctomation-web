a8660381ab3a4fb69dfc57a420984277f90a6c48
ealfugene13 <eugenealfonso71394@gmail.com>
2026-08-23 19:43:22 +0800

    Add configuration settings for Supabase functions

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+
+  SUPABASE_FUNCTION_BASE:
+    "https://ithmrccqlmuodylilfsx.supabase.co/functions/v1",
+
+  FACEBOOK_OAUTH_START:
+    "https://ithmrccqlmuodylilfsx.supabase.co/functions/v1/facebook-oauth-start",
+
+  CLIENT_PAGES_ENDPOINT:
+    "https://ithmrccqlmuodylilfsx.supabase.co/functions/v1/client-pages",
+
+};
